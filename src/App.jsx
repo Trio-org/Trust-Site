112eb9ccf518d093168527dd1ea11f856f865068
balamurugan634 <bala060304@gmail.com>
2024-07-18 22:45:29 +0530

    feat:carousel component done - bala

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import './App.css'
 import Services from './pages/Services'
 import About from './pages/About'
 import DonatePage from './pages/DonatePage'
+import Carousel from './components/Carousel'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const slides=[
+    "https://e0.pxfuel.com/wallpapers/251/661/desktop-wallpaper-size-nature-s-full-nature-for-your-mobile-tablet-explore-of-nature-beautiful-nature-nature-nature-1600x900.jpg",
+    "https://e0.pxfuel.com/wallpapers/251/661/desktop-wallpaper-size-nature-s-full-nature-for-your-mobile-tablet-explore-of-nature-beautiful-nature-nature-nature-1600x900.jpg",
+    "https://e0.pxfuel.com/wallpapers/251/661/desktop-wallpaper-size-nature-s-full-nature-for-your-mobile-tablet-explore-of-nature-beautiful-nature-nature-nature-1600x900.jpg",
+    "https://e0.pxfuel.com/wallpapers/251/661/desktop-wallpaper-size-nature-s-full-nature-for-your-mobile-tablet-explore-of-nature-beautiful-nature-nature-nature-1600x900.jpg"
+  ]
   return (
     <div >
+      <div className="max-w-lg">
+        <Carousel autoslide={true} interval={1000}>
+          {
+            [
+              ...slides.map((slide,i)=><img src={slide} key={i}/>)
+            ]
+          }
+        </Carousel>
+        </div>
      {/* <Services/> */}
      {/* <About/> */}
-     <DonatePage />
+     {/* <DonatePage /> */}
     </div>
   )
 }
